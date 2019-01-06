e4234f2877817a9c16652636cee74a4a0e2931b6
Joel Ye <joelye9@gmail.com>
2019-01-05 23:23:19 -0500

    minor patch

--- data/prepare_bottom_up_features.sh
+++ data/prepare_bottom_up_features.sh
@@ -2,14 +2,17 @@
 
 wget -O data/visdial_1.0_train.zip https://www.dropbox.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip?dl=1
 unzip data/visdial_1.0_train.zip
+mv visdial_1.0_train.json data/
 rm data/visdial_1.0_train.zip
 
 wget -O data/visdial_1.0_val.zip https://www.dropbox.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip?dl=1
 unzip data/visdial_1.0_val.zip
+mv visdial_1.0_train.json data/
 rm data/visdial_1.0_val.zip
 
 wget -O data/visdial_1.0_test.zip https://www.dropbox.com/s/o7mucbre2zm7i5n/visdial_1.0_test.zip?dl=1
 unzip data/visdial_1.0_test.zip
+mv visdial_1.0_test.json data/
 rm data/visdial_1.0_test.zip
 
 # Dataloader does preprocessing, would rather get raw data from above
@@ -20,8 +23,7 @@ rm data/visdial_1.0_test.zip
 wget -O data/features_faster_rcnn_x101_train.h5 https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_train.h5
 wget -O data/features_faster_rcnn_x101_val.h5 https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_val.h5
 wget -O data/features_faster_rcnn_x101_test.h5 https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/features_faster_rcnn_x101_test.h5
-wget -O data/visdial_1.0_word_counts_train.json
-https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/visdial_1.0_word_counts_train.json
+wget -O data/visdial_1.0_word_counts_train.json https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/visdial_1.0_word_counts_train.json
 
 
 # Below is discarded for generating/combining actual features
@@ -54,7 +56,7 @@ https://s3.amazonaws.com/visual-dialog/data/v1.0/2019/visdial_1.0_word_counts_tr
 # python train.py --config-yml lf_disc_faster_rcnn_x101_bs32.yml --validate
 
 # To run eval on validation (20 epochs) - sanity check this
-# python evaluate.py --config-yml lf_disc_faster_rcnn_x101_bs32.yml --use-gt true --load-pthpath checkpoints/checkpoint_19.pth
+# python evaluate.py --config-yml configs/lf_disc_faster_rcnn_x101_bs32.yml --use-gt true --load-pthpath checkpoints/checkpoint_19.pth
 
 # Run eval on test
-# python evaluate.py --config-yml lf_disc_faster_rcnn_x101_bs32.yml --use-gt false --load-pthpath checkpoints/checkpoint_19.pth --eval-json data/visdial_1.0_test.json
+# python evaluate.py --config-yml configs/lf_disc_faster_rcnn_x101_bs32.yml --use-gt false --load-pthpath checkpoints/checkpoint_19.pth --eval-json data/visdial_1.0_test.json
